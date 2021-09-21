fdb5edd47cb4f66f90e3c7d2f0317b26066f44e6
rafaeldss <rafaeldss199916@gmail.com>
2021-09-20 23:31:46 -0300

    Atividade 12

---
+++ atividades/uec/src/main/java/br/uec/Uec.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uec;
+
+/**
+ *
+ * @author rafael
+ */
+public class Uec {
+    public static void main(String[] args) {
+         Lutador lutadores[] = new Lutador[6];
+               
+        // Lutadores
+        lutadores[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        lutadores[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        lutadores[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.09f, 12, 2, 1);
+        lutadores[3] = new Lutador("Dead Code", "Austrália" , 28, 1.93f, 81.6f, 13, 0, 2);
+        lutadores[4] = new Lutador("UFOCobol","Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        lutadores[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
+        
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(lutadores[4], lutadores[5]);
+        UEC01.lutar();
+    }
+}
